3602e76e923232eefd8438fa231d1b9ccc53ee2f
GSM-Godwin <godwinaliu29@gmail.com>
2025-05-21 15:59:53 +0100

    removed header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Header from './components/Header';
 import Hero from './components/Hero';
 import WhatWeOffer from './components/WhatWeOffer';
 
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen max-w-[1440px]">
-      {/* <Header /> */}
       <Hero />
       <WhatWeOffer />
 
